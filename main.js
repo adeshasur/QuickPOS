8ffc0d47fcb5d0b6101cf3bec61a3c7b0f509f58
Adheesha Sooriyaarachchi <106448404+adeshasur@users.noreply.github.com>
2026-05-02 17:45:19 +0530

    Initial commit: Electron app setup with premium UI template and .gitignore

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    title: 'QuickPOS - Supermarket System',
+  });
+
+  win.loadFile(path.join(__dirname, 'src/index.html'));
+  
+  // Open DevTools during development
+  // win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
